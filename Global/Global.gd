cce48fa1fd244d6e90d9afe509d8bca1a5cb54bf
juanantoniogit <juanantonio.tecno@gmail.com>
2019-06-18 11:30:55 +0200

    Add files via upload

---
+++ Global/Global.gd
@@ -0,0 +1,36 @@
+extends Node
+
+var NombreRueda = null
+var numHoras = null
+var numUser = null
+#__________________________________
+var nombreUser = null
+
+var entradaL = null
+var entradaM = null
+var entradaX = null
+var entradaJ = null
+var entradaV = null
+#__________________________________
+var salidaL = null
+var salidaM = null
+var salidaX = null
+var salidaJ = null
+var salidaV = null
+#__________________________________
+var usuario ={
+	entradaL = null,
+	entradaM = null,
+	entradaX = null,
+	entradaJ = null,
+	entradaV = null,
+	
+	salidaL = null,
+	salidaM = null,
+	salidaX = null,
+	salidaJ = null,
+	salidaV = null
+	}
+#__________________________________
+
+
---
+++ Menu principal/MenuPincipal.gd
@@ -0,0 +1,17 @@
+extends Control
+var Configuracion = preload("res://Configuracion/Configuracion.tscn").instance()
+
+
+func _ready():
+
+	pass
+
+#func _process(delta):
+
+#	pass
+
+
+func _on_NuevaRueda_pressed():
+	get_node("/root/").add_child(Configuracion)
+	$".".queue_free()
+	pass # replace with function body
