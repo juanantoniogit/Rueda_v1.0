39838f57d329f42a3d5da88cd2d49fdf1b2e59f2
juanantoniogit <juanantonio.tecno@gmail.com>
2019-06-20 14:21:15 +0200

    Update Config Usuario.gd

--- Configuracion/configuracion de usuario/Config Usuario.gd
+++ Configuracion/configuracion de usuario/Config Usuario.gd
@@ -2,21 +2,29 @@ extends Control
 
 onready var horas = Global.numHoras
 onready var nombre = Global.nombreUser
-var horaSalida = load("res://Configuracion/configuracion de usuario/Hora de salida/Hora de salida.tscn").instance()
-var horaEntrada = load("res://Configuracion/configuracion de usuario/Hora de entrada/Hora de entrada.tscn").instance()
+#var horaSalida = load("res://Configuracion/configuracion de usuario/Hora de salida/Hora de salida.tscn").instance()
+#var horaEntrada = load("res://Configuracion/configuracion de usuario/Hora de entrada/Hora de entrada.tscn").instance()
 
 func _ready():
 	horasEntrada()
 	horasSalida()
+	Nombre()
 	pass	
 func horasEntrada():
 	for i in range (horas):
-#		horaEntrada.set_name(i)
-		$PanelContainer/HBoxContainer/entrada.add_child(horaEntrada)
-#		var botonHora = get_node("/root/Configuracion/Usuarios/VBoxContainer/Config Usuario/PanelContainer/HBoxContainer/entrada/Hora de entrada/Button")
-#		botonHora.set_text(i)
+		var BotonEntrada = Button.new()
+		BotonEntrada.set_text(str(i+1))
+		BotonEntrada.set_name(str(i+1))
+		$PanelContainer/HBoxContainer/entrada.add_child(BotonEntrada)
+		print(i)
 	pass
 func horasSalida():
-	for i in range (6):
-		$PanelContainer/HBoxContainer/salida.add_child(horaSalida)
+	for i in range (horas):
+		var BotonSalida = Button.new()
+		BotonSalida.set_text(str(i+1))
+		BotonSalida.set_name(str(i+1))
+		$PanelContainer/HBoxContainer/salida.add_child(BotonSalida)
+	pass
+func Nombre():
+	$PanelContainer/HBoxContainer/Nombre.set_text(nombre)
 	pass
