2bffbb1c9818cf858ecda6a7ce95b733fcf271f3
juanantoniogit <juanantonio.tecno@gmail.com>
2019-06-30 11:24:30 +0200

    añado dias de la semana

--- Configuracion/configuracion de usuario/Config Usuario.gd
+++ Configuracion/configuracion de usuario/Config Usuario.gd
@@ -4,44 +4,61 @@ onready var horas = Global.numHoras
 onready var nombre = Global.nombreUser
 #var horaSalida = load("res://Configuracion/configuracion de usuario/Hora de salida/Hora de salida.tscn").instance()
 #var horaEntrada = load("res://Configuracion/configuracion de usuario/Hora de entrada/Hora de entrada.tscn").instance()
-var a = null
+var lunes = "lunes"
+var martes = "martes"
+var miercoles = "miercoles"
+var jueves = "jueves"
+var viernes = "viernes"
+var semana = [lunes, martes, miercoles, jueves, viernes]
 func _ready():
-	horasEntrada()
-	horasSalida()
+	horasEntrada(lunes)
+	horasSalida(lunes)
+	horasEntrada(martes)
+	horasSalida(martes)
+	horasEntrada(miercoles)
+	horasSalida(miercoles)
+	horasEntrada(jueves)
+	horasSalida(jueves)
+	horasEntrada(viernes)
+	horasSalida(viernes)
+	
 	Nombre()
 	pass	
 
 
 #__________________________________Establece el nº de botones segun nº horas entrada
-func horasEntrada():
+func horasEntrada(dia):
 	for i in range (horas):
 		var BotonEntrada = Button.new()
 		BotonEntrada.set_text(str(i+1))
 		BotonEntrada.set_name(str(i+1))
-		$PanelContainer/HBoxContainer/entrada.add_child(BotonEntrada)
-		BotonEntrada.connect("pressed", self, "horaEntradaSelect",[i+1])
+		var ruta = "PanelContainer/HBoxContainer/semana/" + str(dia) + "/" + str(dia) + "/entrada"
+		get_node(ruta).add_child(BotonEntrada)
+		BotonEntrada.connect("pressed", self, "horaEntradaSelect",[i+1, dia])
 	pass
-	#_________	
-func horaEntradaSelect(i):
-	print ("holaEntrada" +str(i))
+	#_________guarda las horas y dias seleccionados en cada usuario
+func horaEntradaSelect(i, dia):
+	print ("horaEntrada" + str(i) + str(dia))
 	pass
 #__________________________________Establece el nº de botones segun nº horas salida
-func horasSalida():
+func horasSalida(dia):
 	for i in range (horas):
 		var BotonSalida = Button.new()
 		BotonSalida.set_text(str(i+1))
 		BotonSalida.set_name(str(i+1))
-		$PanelContainer/HBoxContainer/salida.add_child(BotonSalida)
+		var ruta = "PanelContainer/HBoxContainer/semana/" + str(dia) + "/" + str(dia) + "/salida"
+		get_node(ruta).add_child(BotonSalida)
 		var funcionName = "horaSalidaSelec" + str(i+1)
-		BotonSalida.connect("pressed", self, "horaSalidaSelect",[i+1])
+		BotonSalida.connect("pressed", self, "horaSalidaSelect",[i+1,dia])
 	pass
-func horaSalidaSelect(i):
-	print ("holaSalida" +str(i))
+	#_________guarda las horas y dias seleccionados en cada usuario
+func horaSalidaSelect(i, dia):
+	print ("horaSalida" + str(i) + str(dia))
+	
 	pass
 #__________________________________Pone el nombre del usuario a editar	
 func Nombre():
 	$PanelContainer/HBoxContainer/Nombre.set_text(nombre)
-	
 	pass
 
 
